60da666afe6aaaa41147d4ef14016e8d2f4a7d47
yt <3535673677@qq.com>
2026-09-13 12:43:57 +0800

    Site updated: 2026-09-13 12:43:50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1789274568159";const preCache = ["/images/taichi.png","/images/theme/banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1789274624026";const preCache = ["/images/taichi.png","/images/theme/banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
